dec5a9aa3acf68e2a6b29e0c137b91c3adcfc90a
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-09 11:06:32 +0900

    Update SoundManager.cs

--- Assets/Scripts/Singleton/SoundManager.cs
+++ Assets/Scripts/Singleton/SoundManager.cs
@@ -6,7 +6,10 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
 
+    //SoundManager gameobject아래에 bgm, sfx object를 자식으로 둠.
+    // 배경음은 거리상관없이 2d사운드식으로
     [SerializeField] AudioSource bgm;
+    //레벨업 같은 소리는 2d사운드식으로 거리상관없이 그냥 들리도록하기위해서
     [SerializeField] AudioSource sfx;
 
     private void Awake()
@@ -50,5 +53,18 @@ public class SoundManager : MonoBehaviour
     {
         bgm.volume = volume;
         bgm.pitch = pitch;
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfx.PlayOneShot(clip);
+        sfx.clip = clip;
+        sfx.Play();
+    }
+
+    public void SetSFX(float volume, float pitch = 1f)
+    {
+        sfx.volume = volume;
+        sfx.pitch = pitch;
     }
 }
