d338849f4e288c0fc8b4fd7feab826001bbb569f
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-10-14 17:14:31 +0900

    InitManager 추가
    InitManager 추가

---
+++ Assets/Scripts/Singleton/InitManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitManager : MonoBehaviour
+{
+    public static GameManager Game { get { return GameManager.Instance; } }
+
+    //게임 시작하면 다른것보다 가장 먼저 호출될 함수
+    //용도: 게임 설정, 싱글톤 생성, 등
+    //Manager 방식 말고도 Resources 폴더 내부에 프리펩으로 저장하는 방식도 좋음.
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initialize()
+    {
+        GameObject.Instantiate(Resources.Load<GameManager>("Manager/GameManager"));
+    }
+}
